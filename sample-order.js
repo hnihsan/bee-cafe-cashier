312db91f31a5f92295333e23c2db95680cd26ee9
Helmi Nur Ihsan <helmi.n.ihsan@gmail.com>
2022-03-16 16:16:03 +0700

    add swarm implementation

---
+++ sample-order.js
@@ -0,0 +1,69 @@
+const response = {
+    orders: [
+      {
+        category: 'coffee',
+        item: {
+          name: 'Caramel Macchiato',
+          price: '10',
+          image:
+            'https://gateway-proxy-bee-8-0.gateway.ethswarm.org/bzz/cac57098810ea60163e02fcfe017c207fd86ae99cff4001645fa171e6c4db5c7/',
+        },
+        qty: 2,
+        subtotal: 20,
+        notes: '',
+      },
+      {
+        category: 'teas',
+        item: {
+          name: 'Black Tea',
+          price: '7',
+          image:
+            'https://gateway-proxy-bee-2-0.gateway.ethswarm.org/bzz/f7ce35d89b4404d4b1230511c31566704add994e95f4e21ea291c15a206bda78/',
+        },
+        qty: 3,
+        subtotal: 21,
+        notes: '1/4 sugar..',
+      },
+      {
+        category: 'teas',
+        item: {
+          name: 'Green Tea',
+          price: '7',
+          image:
+            'https://gateway-proxy-bee-1-0.gateway.ethswarm.org/bzz/a8d06e2dd8189704d674d846eda871919d95eece82c1641bcefd91145e51c424/',
+        },
+        qty: 2,
+        subtotal: 14,
+        notes: 'No sugar...',
+      },
+      {
+        category: 'juices',
+        item: {
+          name: 'Raspberry Black Currant',
+          price: '8',
+          image:
+            'https://gateway-proxy-bee-1-0.gateway.ethswarm.org/bzz/f837296b3ba8ef8d286cc211fe65676f2b77732dfc59f61eedc6c76877b3ece7/',
+        },
+        qty: 2,
+        subtotal: 16,
+        notes: 'Sweatt...',
+      },
+      {
+        category: 'breakfasts',
+        item: {
+          name: 'Scrambled Egg & Bacon',
+          price: '15',
+          image:
+            'https://gateway-proxy-bee-2-0.gateway.ethswarm.org/bzz/8c90a5c6b3378bd1bc1c1ccc403d05b4ca1c4a02cfa7ef0996f56affed7c0c1c/',
+        },
+        qty: 1,
+        subtotal: 15,
+        notes: 'No crack',
+      },
+    ],
+    total: 86,
+    tax: '8.60',
+    grandTotal: '868.60',
+    customer: 'Nico',
+    tableNo: 2,
+  };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import Board from 'kanban-board-from-react-trello';
 import { Card } from '@styles/global.style';
 import RenderKanbanCard from '@parts/RenderKanbanCard';
+import { Bee } from "@ethersphere/bee-js"
 
 type Props = {};
 
 export default function Home({}: Props) {
+  const beeUrl = "http://localhost:1633";
+  const bee = new Bee(beeUrl);
   const [listOrders, setListOrders] = useState([]);
+  const [orderReference, setOrderReference] = useState("");
 
   const getCategoriesFromOrders = (orders: any) => {
     const categories = [];
@@ -42,105 +46,46 @@ export default function Home({}: Props) {
     ],
   });
 
-  const handleKeyDown = (event) => {
+  const handleKeyDown = async (event) => {
     if (event.key === 'Enter') {
-      const response: any = {
-        orders: [
-          {
-            category: 'coffee',
-            item: {
-              name: 'Caramel Macchiato',
-              price: '10',
-              image:
-                'https://gateway-proxy-bee-8-0.gateway.ethswarm.org/bzz/cac57098810ea60163e02fcfe017c207fd86ae99cff4001645fa171e6c4db5c7/',
-            },
-            qty: 2,
-            subtotal: 20,
-            notes: '',
-          },
-          {
-            category: 'teas',
-            item: {
-              name: 'Black Tea',
-              price: '7',
-              image:
-                'https://gateway-proxy-bee-2-0.gateway.ethswarm.org/bzz/f7ce35d89b4404d4b1230511c31566704add994e95f4e21ea291c15a206bda78/',
-            },
-            qty: 3,
-            subtotal: 21,
-            notes: '1/4 sugar..',
-          },
-          {
-            category: 'teas',
-            item: {
-              name: 'Green Tea',
-              price: '7',
-              image:
-                'https://gateway-proxy-bee-1-0.gateway.ethswarm.org/bzz/a8d06e2dd8189704d674d846eda871919d95eece82c1641bcefd91145e51c424/',
-            },
-            qty: 2,
-            subtotal: 14,
-            notes: 'No sugar...',
-          },
-          {
-            category: 'juices',
-            item: {
-              name: 'Raspberry Black Currant',
-              price: '8',
-              image:
-                'https://gateway-proxy-bee-1-0.gateway.ethswarm.org/bzz/f837296b3ba8ef8d286cc211fe65676f2b77732dfc59f61eedc6c76877b3ece7/',
-            },
-            qty: 2,
-            subtotal: 16,
-            notes: 'Sweatt...',
-          },
-          {
-            category: 'breakfasts',
-            item: {
-              name: 'Scrambled Egg & Bacon',
-              price: '15',
-              image:
-                'https://gateway-proxy-bee-2-0.gateway.ethswarm.org/bzz/8c90a5c6b3378bd1bc1c1ccc403d05b4ca1c4a02cfa7ef0996f56affed7c0c1c/',
-            },
-            qty: 1,
-            subtotal: 15,
-            notes: 'No crack',
-          },
-        ],
-        total: 86,
-        tax: '8.60',
-        grandTotal: '868.60',
-        customer: 'Nico',
-        tableNo: 2,
-      };
-      response.id = `${response.tableNo}-${response.customer}`;
+      try {
+        const swarmData = await bee.downloadData(orderReference);
+        const response : any = swarmData.json()
 
-      const isOrderExist = listOrders.find((order) => order.id === response.id);
-      if (!isOrderExist) {
-        const categories = getCategoriesFromOrders(response.orders);
+        const isOrderExist = listOrders.find((order) => order.id === response.id);
+        if (!isOrderExist) {
+          const categories = getCategoriesFromOrders(response.orders);
 
-        data.lanes[0].cards.push({
-          id: response.id,
-          title: `[# ${response.tableNo ?? '00'}] ${response.customer ?? '-'}`,
-          description: (
-            <RenderKanbanCard
-              data={{
-                categories,
-                items: response.orders,
-              }}
-            />
-          ),
-          label: `${response.orders.length} Items`,
-          laneId: 'new-order-lane',
-        });
-        data.lanes[0].label = data.lanes[0].cards.length.toString();
+          data.lanes[0].cards.push({
+            id: response.id,
+            title: `[# ${response.tableNo ?? '00'}] ${response.customer ?? '-'}`,
+            description: (
+              <RenderKanbanCard
+                data={{
+                  categories,
+                  items: response.orders,
+                }}
+              />
+            ),
+            label: `${response.orders.length} Items`,
+            laneId: 'new-order-lane',
+          });
+          data.lanes[0].label = data.lanes[0].cards.length.toString();
 
-        setData({ ...data });
-        setListOrders([...listOrders, response]);
-      } else {
+          setData({ ...data });
+          setListOrders([...listOrders, response]);
+        } else {
+          alert(
+            `Order for [# ${isOrderExist.tableNo}] ${isOrderExist.customer} already exist`
+          );
+        }
+      } catch (error) {
+        console.log(error);
         alert(
-          `Order for [# ${isOrderExist.tableNo}] ${isOrderExist.customer} already exist`
+          `Invalid Order Reference`
         );
+      } finally {
+        setOrderReference("");
       }
     }
   };
@@ -165,7 +110,9 @@ export default function Home({}: Props) {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xs placeholder:text-xs"
           type="text"
           placeholder="Enter Order Reference"
+          value={orderReference}
           onKeyDown={handleKeyDown}
+          onChange={(e) => setOrderReference(e.currentTarget.value)}
         />
       </Card>
 
